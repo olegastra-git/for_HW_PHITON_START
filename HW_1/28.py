b14c83799d7e4f42f831143527d653e799a4aeef
olegastra-git <qwertyray1995@gmail.com>
2023-03-25 21:53:59 +0300

    Домашняя работа задача 28

---
+++ HW_1/28.py
@@ -0,0 +1,7 @@
+# домашн€€ работа по phiton задача 28
+
+a = int(input(" vvedite chislo a: - "))
+b = int(input(" vvedite chislo b: - "))
+def summa(a, b):
+    return summa(a + 1, b - 1) if b > 0 else
+   print sum(a, b)
