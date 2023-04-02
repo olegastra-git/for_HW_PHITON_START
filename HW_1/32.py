cb91fd2aa677036d37a9c91318c80ff1d7289e8d
olegastra-git <112951133+olegastra-git@users.noreply.github.com>
2023-04-03 00:00:07 +0300

    Delete HW_1 directory

--- HW_1/32.py
+++
@@ -1,6 +0,0 @@
-list = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-min = int(input('vveite minimalnoe chslo: '))
-max = int(input('vveite maksimalnoechslo: '))
-for i in range(len(list)):
-    if min <= list[i] <= max:
-        print(i)
